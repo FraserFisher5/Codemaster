ace0396f717d7018f34b853e82ad5d58f25338f3
FraserFisher5 <130382982+FraserFisher5@users.noreply.github.com>
2023-06-16 10:59:52 +0200

    Create sax.py

---
+++ sax.py
@@ -0,0 +1,9 @@
+# https://github.com/MadelineMeerab1/Codemaster
+import numpy
+In [9]: import numpy as np
+
+In [10]: dot_product_1 = np.dot(input_vector, weights_1)
+
+In [11]: print(f"The dot product is: {dot_product_1}")
+Out[11]: The dot product is: 2.1672
+    
